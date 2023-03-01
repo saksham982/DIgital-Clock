74bccab301ff250ffd67afec66a72c8a78c5cc62
Saksham <NP03CS4A220167@heraldcollege.edu.np>
2023-03-01 21:28:24 +0545

    Digital_Clock

---
+++ script.js
@@ -0,0 +1,17 @@
+let hours;
+let a;
+let min;
+let date;
+setInterval(() => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+     a= new Date();
+    hours=a.getHours()
+    min=a.getMinutes();
+    if (min <10){
+        min ="0"+min;
+    }
+    date=a.toLocaleDateString(undefined, options)
+    document.getElementById("hour").innerText=hours;   
+    document.getElementById("min").innerText=min; 
+    document.getElementById("date").innerText=date; 
+}, 1000);
